18ce9c3fc1d2f8f3e4bb2b8815f30b08e078fbbd
siam8900-ux <smfb8900@gmail.com>
2026-07-26 13:39:03 +0600

    Update adminonly.js

--- scripts/cmds/adminonly.js
+++ scripts/cmds/adminonly.js
@@ -39,31 +39,50 @@ module.exports = {
 	},
 
 	onStart: function ({ args, message, getLang }) {
-		let isSetNoti = false;
-		let value;
-		let indexGetVal = 0;
+	let isSetNoti = false;
+	let value;
+	let indexGetVal = 0;
 
-		if (args[0] == "noti") {
-			isSetNoti = true;
-			indexGetVal = 1;
-		}
-
-		if (args[indexGetVal] == "on")
-			value = true;
-		else if (args[indexGetVal] == "off")
-			value = false;
-		else
-			return message.SyntaxError();
-
-		if (isSetNoti) {
-			config.hideNotiMessage.adminOnly = !value;
-			message.reply(getLang(value ? "turnedOnNoti" : "turnedOffNoti"));
-		}
-		else {
-			config.adminOnly.enable = value;
-			message.reply(getLang(value ? "turnedOn" : "turnedOff"));
-		}
-
-		fs.writeFileSync(client.dirConfig, JSON.stringify(config, null, 2));
+	if (args[0] == "noti") {
+		isSetNoti = true;
+		indexGetVal = 1;
 	}
-};
+
+	if (args[indexGetVal] == "on")
+		value = true;
+	else if (args[indexGetVal] == "off")
+		value = false;
+	else
+		return message.SyntaxError();
+
+
+	if (isSetNoti) {
+
+		config.hideNotiMessage.adminOnly = !value;
+		global.GoatBot.config.hideNotiMessage.adminOnly = !value;
+
+		message.reply(
+			getLang(value ? "turnedOnNoti" : "turnedOffNoti")
+		);
+
+	}
+
+	else {
+
+		config.adminOnly.enable = value;
+
+		// Important fix
+		global.GoatBot.config.adminOnly.enable = value;
+
+		message.reply(
+			getLang(value ? "turnedOn" : "turnedOff")
+		);
+
+	}
+
+
+	fs.writeFileSync(
+		client.dirConfig,
+		JSON.stringify(config, null, 2)
+	);
+}
