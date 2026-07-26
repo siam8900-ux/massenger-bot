e16ccdb700d30c3cf67ee7ea97449f88a1144a7d
siam8900-ux <smfb8900@gmail.com>
2026-07-26 13:20:48 +0600

    Update kick.js

--- scripts/cmds/kick.js
+++ scripts/cmds/kick.js
@@ -1,56 +1,43 @@
 module.exports = {
 	config: {
 		name: "kick",
-		version: "1.3",
-		author: "NTKhang",
+		version: "1.4",
+		author: "SIAM",
 		countDown: 5,
 		role: 1,
-		description: {
-			vi: "Kick thành viên khỏi box chat",
-			en: "Kick member out of chat box"
-		},
-		category: "box chat",
-		guide: {
-			vi: "   {pn} @tags: dùng để kick những người được tag",
-			en: "   {pn} @tags: use to kick members who are tagged"
-		}
+		category: "box chat"
 	},
 
-	langs: {
-		vi: {
-			needAdmin: "Vui lòng thêm quản trị viên cho bot trước khi sử dụng tính năng này"
-		},
-		en: {
-			needAdmin: "Please add admin for bot before using this feature"
-		}
-	},
+	onStart: async function ({ api, message, event }) {
 
-	onStart: async function ({ message, event, args, threadsData, api, getLang }) {
-		const adminIDs = await threadsData.get(event.threadID, "adminIDs");
-		if (!adminIDs.includes(api.getCurrentUserID()))
-			return message.reply(getLang("needAdmin"));
-		async function kickAndCheckError(uid) {
-			try {
-				await api.removeUserFromGroup(uid, event.threadID);
-			}
-			catch (e) {
-				message.reply(getLang("needAdmin"));
-				return "ERROR";
-			}
-		}
-		if (!args[0]) {
-			if (!event.messageReply)
-				return message.SyntaxError();
-			await kickAndCheckError(event.messageReply.senderID);
-		}
-		else {
-			const uids = Object.keys(event.mentions);
-			if (uids.length === 0)
-				return message.SyntaxError();
-			if (await kickAndCheckError(uids.shift()) === "ERROR")
-				return;
-			for (const uid of uids)
-				api.removeUserFromGroup(uid, event.threadID);
+		if (!event.messageReply && Object.keys(event.mentions).length == 0)
+			return message.reply("⚠️ Reply or tag someone to kick.");
+
+		let uid;
+
+		if (event.messageReply)
+			uid = event.messageReply.senderID;
+		else
+			uid = Object.keys(event.mentions)[0];
+
+		try {
+
+			await api.removeUserFromGroup(
+				uid,
+				event.threadID
+			);
+
+			return message.reply(
+				"✅ Member removed successfully."
+			);
+
+		} catch (err) {
+
+			console.log(err);
+
+			return message.reply(
+				"❌ Cannot kick member.\nMake sure bot is admin."
+			);
 		}
 	}
 };
